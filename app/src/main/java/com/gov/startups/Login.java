abfdda0574798077727379e3e5592dba80b19ebe
Marwan Alramahi <m.alramahi98@gmail.com>
2019-07-05 15:45:11 +0300

    Added Login activity.

---
+++ app/src/main/java/com/gov/startups/Login.java
@@ -0,0 +1,14 @@
+package com.gov.startups;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/gov/startups/MainActivity.java
+++ app/src/main/java/com/gov/startups/MainActivity.java
@@ -2,7 +2,9 @@ package com.gov.startups;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clicked(View view) {
+        Intent lgn = new Intent(getApplicationContext(), Login.class);
+        if (view.getTag().toString().equals("company")) {
+            lgn.putExtra("type", 0);
+            startActivity(lgn);
+        } else if (view.getTag().toString().equals("gov")) {
+            lgn.putExtra("type", 1);
+            startActivity(lgn);
+        } else if (view.getTag().toString().equals("sponeser")) {
+            lgn.putExtra("type", 2);
+            startActivity(lgn);
+        }
     }
 }
