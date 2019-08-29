08e1328a241499a9eff32427e2eae9ca680ae0df
Marwan998 <m.alramahi98@gmail.com>
2019-08-29 15:49:58 +0300

    -Designed sponsor main.

--- app/src/main/java/com/gov/startups/SponsorMain.java
+++ app/src/main/java/com/gov/startups/SponsorMain.java
@@ -3,9 +3,15 @@ package com.gov.startups;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SponsorMain extends AppCompatActivity {
 
@@ -14,12 +20,27 @@ public class SponsorMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_sponsor);
 
+        ImageView iv = findViewById(R.id.imageView);
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        iv.getLayoutParams().height=size.y/2;
+
         Button btn = findViewById(R.id.button5);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                v.setBackgroundResource(R.drawable.btn_design_clicked);
                 startActivity(new Intent(SponsorMain.this, searchSponsor.class));
+                final Button temp = (Button)v;
+                Timer timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        temp.setBackgroundResource(R.drawable.btn_design);
+                    }
+                },500);
             }
         });
 
@@ -29,10 +50,19 @@ public class SponsorMain extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                v.setBackgroundResource(R.drawable.btn_design_clicked);
                 Intent intent = new Intent(SponsorMain.this,Spon_Messages.class);
                 intent.putExtra("name",getIntent().getStringExtra("name"));
                 intent.putExtra("type",1);
                 startActivity(intent);
+                final Button temp = (Button)v;
+                Timer timer = new Timer();
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        temp.setBackgroundResource(R.drawable.btn_design);
+                    }
+                },500);
             }
         });
 
@@ -41,10 +71,19 @@ public class SponsorMain extends AppCompatActivity {
          btn2.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View view) {
+                 view.setBackgroundResource(R.drawable.btn_design_clicked);
                  Intent intent = new Intent(SponsorMain.this,Spon_Messages.class);
                  intent.putExtra("name",getIntent().getStringExtra("name"));
                  intent.putExtra("type",3);
                  startActivity(intent);
+                 final Button temp = (Button)view;
+                 Timer timer = new Timer();
+                 timer.schedule(new TimerTask() {
+                     @Override
+                     public void run() {
+                         temp.setBackgroundResource(R.drawable.btn_design);
+                     }
+                 },500);
              }
          });
 
@@ -53,7 +92,16 @@ public class SponsorMain extends AppCompatActivity {
          btn3.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View view) {
+                 view.setBackgroundResource(R.drawable.btn_design_clicked);
                  startActivity(new Intent(SponsorMain.this,seeGovermentAds.class));
+                 final Button temp = (Button)view;
+                 Timer timer = new Timer();
+                 timer.schedule(new TimerTask() {
+                     @Override
+                     public void run() {
+                         temp.setBackgroundResource(R.drawable.btn_design);
+                     }
+                 },500);
              }
          });
     }
