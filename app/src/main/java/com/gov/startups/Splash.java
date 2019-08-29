9bbb5736d2a28c69e8240596917ed5b0b4750562
Marwan998 <m.alramahi98@gmail.com>
2019-08-29 17:47:54 +0300

    -Added splash screen

---
+++ app/src/main/java/com/gov/startups/Splash.java
@@ -0,0 +1,27 @@
+package com.gov.startups;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
